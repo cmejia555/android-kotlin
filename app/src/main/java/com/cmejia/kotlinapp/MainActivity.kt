cba74ced2a2b8d282559d5b1826f10da3757cf50
cmejia <cesarmejia555@yahoo.com.ar>
2020-04-12 20:15:11 -0300

    Set all behave to buttons

--- app/src/main/java/com/cmejia/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/cmejia/kotlinapp/MainActivity.kt
@@ -2,18 +2,18 @@ package com.cmejia.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.TypedValue
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.ToggleButton
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var messageTextView : TextView
     lateinit var upSizeButton: Button
     lateinit var downSizeButton: Button
-    lateinit var colorButton: Button
     lateinit var toggleButton: Button
+    lateinit var colorButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +22,8 @@ class MainActivity : AppCompatActivity() {
         messageTextView = findViewById(R.id.tv_message)
         upSizeButton = findViewById(R.id.btn_up)
         downSizeButton = findViewById(R.id.btn_down)
-        colorButton = findViewById(R.id.btn_change_color)
         toggleButton = findViewById(R.id.btn_toggle)
+        colorButton = findViewById(R.id.btn_change_color)
 
         messageTextView.text = "Bienvenido"
 
@@ -37,6 +37,18 @@ class MainActivity : AppCompatActivity() {
         downSizeButton.setOnClickListener {
             size -= 1
             messageTextView.textSize = size
+        }
+
+        toggleButton.setOnClickListener {
+            when (messageTextView.visibility){
+                View.VISIBLE -> messageTextView.visibility = View.INVISIBLE
+                View.INVISIBLE -> messageTextView.visibility = View.VISIBLE
+                else -> messageTextView.visibility = View.GONE
+            }
+        }
+
+        colorButton.setOnClickListener {
+            messageTextView.setTextColor(resources.getColor(R.color.colorPrimaryDark))
         }
     }
 }
