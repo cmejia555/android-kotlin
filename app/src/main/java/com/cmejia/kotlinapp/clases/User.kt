72b9d2e8f700807058b311b78ef916a0aec9a509
cmejia <cesarmejia555@yahoo.com.ar>
2020-04-13 01:32:01 -0300

    Creacion de la clase User

---
+++ app/src/main/java/com/cmejia/kotlinapp/clases/User.kt
@@ -0,0 +1,12 @@
+package com.cmejia.kotlinapp.clases
+
+class User (username : String, email: String, password : String) {
+
+    val username = username
+    val email : String = email
+    val password: String = password
+
+    override fun toString(): String {
+        return "Usuario creado: user = $username, email = $email"
+    }
+}
