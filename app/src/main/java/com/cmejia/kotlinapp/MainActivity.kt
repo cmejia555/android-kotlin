f64570624a132fba51fe565af92b2aa435adaaa1
cmejia <cesarmejia555@yahoo.com.ar>
2020-04-13 23:57:54 -0300

    Eliminacion de creacion de usuario

--- app/src/main/java/com/cmejia/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/cmejia/kotlinapp/MainActivity.kt
@@ -2,18 +2,12 @@ package com.cmejia.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cmejia.kotlinapp.clases.User
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var user : User
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        user = User("cmejia", "cmejia@google.com", "abcd")
-
     }
 }
