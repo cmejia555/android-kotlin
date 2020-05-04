9d3d96cdae03741ebbb2cc01f428f4f75b5627ba
cmejia <cesarmejia555@yahoo.com.ar>
2020-05-04 16:29:54 -0300

    Creacion del viewPager y su adapter junto con tabs

---
+++ app/src/main/java/com/cmejia/kotlinapp/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.cmejia.kotlinapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.cmejia.kotlinapp.fragments.DetailsFragment
+import com.cmejia.kotlinapp.fragments.MapFragment
+
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> DetailsFragment()
+            1 -> MapFragment()
+            else -> DetailsFragment()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return TAB_LAYOUT_COUNT
+    }
+
+    companion object {
+        private const val TAB_LAYOUT_COUNT = 2
+    }
+
+}
