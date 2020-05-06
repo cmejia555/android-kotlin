cafb3c885fdd025c770981837038c6efbc32c19b
cmejia <cesarmejia555@yahoo.com.ar>
2020-05-06 02:40:59 -0300

    Configuracion de la toolbar en distintos fragments

--- app/src/main/java/com/cmejia/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/cmejia/kotlinapp/MainActivity.kt
@@ -2,34 +2,24 @@ package com.cmejia.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toolbar: Toolbar
-    private lateinit var navController : NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    //lateinit var toolbar: Toolbar
+    //private lateinit var navController : NavController
+    //private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        navController = findNavController(R.id.main_navhost)
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.loginFragment, R.id.listFragment))
-        toolbar.setupWithNavController(navController, appBarConfiguration)
+        //val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        //setSupportActionBar(toolbar)
+        //val navController = findNavController(R.id.main_navhost)
+        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.loginFragment, R.id.listFragment))
+        //toolbar.setupWithNavController(navController, appBarConfiguration)
         //setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
